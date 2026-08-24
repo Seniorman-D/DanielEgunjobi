99b2d2d3fd95c83ffbf839eb427060a5412fed95
Seniorman-D <daniel@yourstudypath.com>
2026-08-24 08:54:58 +0100

    Commit 71 - Add Anyiko upload history interface

---
+++ mzuka-kibao-mp3-manager/frontend/src/components/AnyikoUploadHistory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AnyikoUploadHistory(){
+  return (
+    <div className="anyiko-upload-history">
+      <h3>Upload History</h3>
+      <p>Track previous uploads, replacements and versions.</p>
+      <button>Restore Version</button>
+    </div>
+  );
+}

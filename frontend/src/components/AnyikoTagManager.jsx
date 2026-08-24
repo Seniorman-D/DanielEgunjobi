4677524a1abaeeedfdee26b138f792767682f963
Seniorman-D <daniel@yourstudypath.com>
2026-08-24 01:48:12 +0100

    Commit 55 - Anyiko tag management interface

---
+++ frontend/src/components/AnyikoTagManager.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AnyikoTagManager(){
+  return (
+    <div className="anyiko-tag-manager">
+      <h2>Anyiko Tag Management</h2>
+      <p>Create, edit and manage genres, artists, albums and media tags.</p>
+      <button>Create Tag</button>
+    </div>
+  );
+}

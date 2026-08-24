0076d9bb376286d64205c83fc880b2dcbb6c155c
Seniorman-D <daniel@yourstudypath.com>
2026-08-24 01:18:08 +0100

    Add tag management UI component

---
+++ mzuka-kibao-mp3-manager/frontend/src/components/TagManager.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function TagManager(){
+  return (
+    <section>
+      <h1>Tag Management System</h1>
+      <input placeholder="Create or search tags" />
+      <button>Add Tag</button>
+    </section>
+  );
+}

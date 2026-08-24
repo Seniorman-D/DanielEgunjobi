80f1457beb17238fc4968eed55f6b8352b8bcd18
Seniorman-D <daniel@yourstudypath.com>
2026-08-24 08:56:53 +0100

    Commit 73 - Anyiko Bulk File Management UI

---
+++ frontend/src/components/AnyikoBulkFileManager.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function AnyikoBulkFileManager() {
+  return (
+    <div className="anyiko-bulk-manager">
+      <h2>Bulk File Management</h2>
+      <button>Select Multiple Files</button>
+      <button>Bulk Rename</button>
+      <button>Bulk Delete</button>
+      <button>Bulk Update Tags</button>
+      <button>Generate Download URLs</button>
+    </div>
+  );
+}
